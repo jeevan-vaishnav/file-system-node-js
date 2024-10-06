1c58d73cb53c5d79d797ddfe464a9d3bf0009e0e
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-06 14:36:31 +0530

    file system wip

--- app.js
+++ app.js
@@ -3,14 +3,31 @@ const fs = require('fs/promises');
 (async () => {
     const commandFileHandler = await fs.open('./command.txt', 'r');
 
+    // This is an event-driven pattern in Node.js, specifically using an event emitter.
+    commandFileHandler.on('change', async () => {
+        // the file was changed
+        console.log("The file was Changed")
+        //get the size of the file
+        const statSize = (await commandFileHandler.stat()).size;
+        //allocate our buffer with size of file
+        const buff = Buffer.alloc(statSize);
+        // the location at which we want to start filling our buffer
+        const offset = 0;
+        //how may bytes we want to read
+        const length = buff.length;
+        //the position that we wnat to starting reading the file from 
+        const position = 0;
+        // We always want to read the whole conent ( form begning all the way to the end)
+        const content = await commandFileHandler.read(buff, offset, length, position);
+        console.log(content);
+    })
+
+    //create a watcher
     const watcher = fs.watch('./command.txt');
     for await (const event of watcher) {
         if (event.eventType === 'change') {
-            // the file was changed
-            console.log("The file was Changed")
-            // we want to read the content of the file
-            const content = await commandFileHandler.read(Buffer.alloc(8));
-            console.log(content);
+            // function emit 
+            commandFileHandler.emit('change');
 
         }
     }
